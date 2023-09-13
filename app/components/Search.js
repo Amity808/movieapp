c80b7835ae16ccd4c6516975aad970255a1ff6dc
Amity808 <bolarinwamuhdsodiq0@gmail.com>
2023-09-13 13:02:07 -0700

    update search bar

--- app/components/Search.js
+++ app/components/Search.js
@@ -2,36 +2,42 @@
 import Image from 'next/image'
 import searBarIcon from "./images/search.svg"
 import { fetchData, movieOptions  } from "../../libs/fetch"
-// import { g } from "../../libs/getSearch"
+import { getSearch } from "../../libs/getSearch"
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
+import MovieCard from './MovieCard'
 
-export default function SearchBar( ) {
+export default  function SearchBar( ) {
   const [search, setSearch] = useState('')
+  const [searchResult, setsearchResult] = useState([])
   
   const router = useRouter();
-
-  let data;
+  
+  // let data;
   const handleSearch = async (e) => {
     e.preventDefault();
-    setSearch('')
-    router.push(`/${search}`)
-
+    // router.push(`/${search}`)
+    
     if(search) {
       const searchUrl = await fetchData(`https://api.themoviedb.org/3/search/movie?query=${search}&include_adult=false&language=en-US&page=1`, movieOptions)
       
 
-      data = await searchUrl
-      console.log(data.results)
+      const data = await searchUrl.results.slice(0, 10)
+      console.log(data)
       // setSearchData(data)
-      setSearch(data)
+      setsearchResult(data)
+      setSearch('')
       // const names = data.results.map(item => item.original_title)
       // console.log(names)
       return data;
     }
   }
+  
+  // const res = await getSearch(search)
+  // const result = await res;
+  console.log(searchResult);
 
-  console.log(search)
+  
   return (
     <div className='flex items-center border-2 border-solid rounded-sm'>
       <form onSubmit={handleSearch}>
@@ -39,6 +45,13 @@ export default function SearchBar( ) {
         <Image src={searBarIcon} alt="Searchicon" className=" relative right-5"/>
       </form>
       {/* <p className=' text-red'> {}</p> */}
+      
+      {searchResult.length > 0 &&  <div className=''>
+        {searchResult.map((movie) => (
+          <MovieCard title={movie.original_title} releaseDate={movie.release_date} popularity={movie.popularity} 
+          posterCard={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`}/>
+        ))}
+      </div> }
     </div>
   )
 }
